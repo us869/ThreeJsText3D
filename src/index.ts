2ae587e11a4f548aa564043444645cde8eb8cff0
us869 <42154943+us869@users.noreply.github.com>
2022-05-06 18:11:59 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { startBasicScene } from './basic_scene';
+//import { startDingbatsScene } from './dingbats_scene';
+//import { startStarWarsScene } from './star_wars_scene'
+
+startBasicScene();
+// startStarWarsScene();
+// startDingbatsScene();
